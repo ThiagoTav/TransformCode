e154a8a45ea3ce15c0fce6a5ea5611965fb51b81
Ana Julia M Martins <ana_juh17@hotmail.com>
2023-02-23 13:01:10 -0300

    main

--- jv/script.js
+++ jv/script.js
@@ -1,3 +1,19 @@
+//Lógica Black and White
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+var data = new Date();
+var horas = data.getHours();
+
+console.log(horas);
+
+window.addEventListener("load", function () {
+  if (horas < 18) {
+    html.classList.toggle("white-mode");
+    body.classList.toggle("white-mode");
+  }
+});
+
+
 //declaração de variáveis
 var content_html = document.getElementById('textarea_hmtl');
 var content_js = document.getElementById('textarea_js');
